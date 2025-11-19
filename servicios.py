f88a1b969670e40cd469921047cf0f726f0fdbdd
Matias Aguirre Correa <matiasaguirrecorrea@gmail.com>
2025-11-19 14:16:58 -0500

    Implement eliminar_estudiante function
    Added a function to remove a student by ID with error handling.

--- servicios.py
+++ servicios.py
@@ -1,9 +1,29 @@
+def eliminar_estudiante():
+    
 
+    if not estudiantes:
+        print("No hay estudiantes por eliminar.")
+        return
 
+    try:
+        id_buscar = int(input("Ingrese el ID del estudiante a eliminar: "))
+    except ValueError:
+        print("El ID debe ser un número.")
+        return
 
+    # Buscar estudiante por ID
+    indice_encontrado = None
 
-
-
+    for i,est in enumerate (estudiantes):
+        if est["id"] == id_buscar:
+            indice_encontrado = i
+            break
+            
+    if indice_encontrado is not None:
+        estudiantes.pop(indice_encontrado)
+        print("Estudiante eliminado exitosamente.")
+    else:
+        print("No se encontró un estudiante con ese ID.")
 
 
 
